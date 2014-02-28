1ce18d991e73f7e03cc5a34ee77d629a9b982350
Niklas Lind <nlin@kth.se>
2014-02-28 17:06:42 +0100

    fixed so that web sockets works now. sending position to server

--- crowdtracker.js
+++ crowdtracker.js
@@ -1,8 +1,10 @@
-
+//HTML server
 var express = require('express');
-var app = express();
+var io = require('socket.io');
+var http = require('http');
+//var app = express();
+var app = express.createServer();
 
- 
 app.configure(function () {
     app.use(express.logger('dev'));
     app.use(express.bodyParser());
@@ -17,16 +19,41 @@ var getAssemblyPoint = function() {
 }
 
 exports.getAssemblyPoint = getAssemblyPoint;
-
+// 
 
 
 app.get('/', function(req, res){
     res.render('index', getAssemblyPoint());
 //     res.sendfile(__dirname + '/views/assemblyPoint.html')
-})
+});
+
+
 
 var port = Number(process.env.PORT || 3000);
-app.listen(port, function() {
-    console.log('Port: ' + port);
+// app.listen(port, function() {
+//     console.log('Port: ' + port);
+// });
+
+//Socket server
+var server = http.createServer(app).listen(port);
+var sio = io.listen(server);
+
+console.log('connecting socket');
+
+
+sio.on('connection', function (socket) {
+    console.log('Got connect');
+    
+    socket.emit('news', { hello: 'world' });
+  
+  socket.on('message', function (data) {
+    console.log('message:' + data);
+  });
+  socket.on('position', function (data) {
+    console.log('position:' + data);
+  });
+
+
 });
+
 
